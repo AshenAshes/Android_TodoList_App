16247b7e416854ff3c2dd7d4f14ea65a9a434ce7
GoldfischeRUA <3170101510@zju.edu.cn>
2019-11-11 11:08:04 +0800

    modify note

--- TodoList/app/src/main/java/com/byted/camp/todolist/beans/Note.java
+++ TodoList/app/src/main/java/com/byted/camp/todolist/beans/Note.java
@@ -5,44 +5,43 @@ import java.util.Date;
 public class Note {
 
     public final long id;
-    private Date date;
-    private State state;
+    private String deadline;
+    private String show;
+    private String scheduled;
+    private int state;
+    private int priority;
+    private int week;
+    private String caption;
+    private String tag;
     private String content;
-    private Priority priority;
 
     public Note(long id) {
         this.id = id;
     }
 
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public State getState() {
-        return state;
-    }
-
-    public void setState(State state) {
-        this.state = state;
-    }
 
     public String getContent() {
         return content;
     }
-
     public void setContent(String content) {
         this.content = content;
     }
+    public String getDeadline(){return deadline;}
+    public void setDeadline(String deadline){this.deadline=deadline; }
+    public String getShow(){return show;}
+    public void setShow(String show){this.show=show;}
+    public String getScheduled(){return scheduled;}
+    public void setScheduled(){this.scheduled=scheduled;}
+    public int getState(){return state;}
+    public void setState(int state){this.state=state;}
+    public int getPriority(){return priority;}
+    public void setPriority(int priority){this.priority=priority;}
+    public int getWeek(){return week;}
+    public void setWeek(int week){this.week=week;}
+    public String getCaption(){return caption;}
+    public void setCaption(String caption){this.caption=caption;}
+    public String getTag(){return tag;}
+    public void setTag(String tag){this.tag=tag;}
 
-    public Priority getPriority() {
-        return priority;
-    }
 
-    public void setPriority(Priority priority) {
-        this.priority = priority;
-    }
 }
